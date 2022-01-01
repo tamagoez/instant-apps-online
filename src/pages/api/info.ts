323afd0fd74a70b0340f2e0e65d39b59124a0863
tamagoes <87574268+tamagoez@users.noreply.github.com>
2022-01-01 16:24:18 +0900

    Update info.ts

--- src/pages/api/info.ts
+++ src/pages/api/info.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 const hello = (req, res) => {
-  res.status(200).json({ sha: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA })
+  res.status(200).json({ sha: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA, message: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_MESSAGE })
 }
 
 export default hello
