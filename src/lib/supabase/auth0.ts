a9bbc744878c254c8fa04dc0d4b5fb548eca0a4c
tamagoes <87574268+tamagoez@users.noreply.github.com>
2022-01-01 23:18:48 +0900

    Create auth0.ts

---
+++ src/lib/supabase/auth0.ts
@@ -0,0 +1,3 @@
+const auth0Secret: string = process.env.NEXT_PUBLIC_AUTH0_SECRET || ''
+const auth0Url: string = process.env.NEXT_PUBLIC_AUTH0_URL || ''
+const auth0IssuerBaseUrl: string = process.env.NEXT_PUBLIC_AUTH0_ISSUER_BASE_URL || ''
