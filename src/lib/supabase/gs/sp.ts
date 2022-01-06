7768d29be672e6f2076a3bd2126351089964d7ef
tamagoes <87574268+tamagoez@users.noreply.github.com>
2022-01-06 16:33:40 +0900

    test?

--- src/lib/supabase/gs/sp.ts
+++ src/lib/supabase/gs/sp.ts
@@ -1,12 +1,14 @@
-import { createClient } from '@supabase/supabase-js'
+// utils/supabase.js
+
+import { createClient } from "@supabase/supabase-js";
 
 const getSupabase = () => {
-    const supabase_ = createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL,
-      process.env.NEXT_PUBLIC_SUPABASE_KEY
-    );
-  
-    return supabase_;
-  };
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_KEY
+  );
 
-export default getSupabase ;
+  return supabase;
+};
+
+export { getSupabase };
