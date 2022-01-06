19b0110f1a80410801ca4fe6d85d2595920f128b
tamagoes <87574268+tamagoez@users.noreply.github.com>
2022-01-06 16:17:18 +0900

    Change `supabase.js` and took backup

--- src/lib/supabase/supabase copy.ts
+++ src/lib/supabase/supabase copy.ts
@@ -1,5 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
+const supabaseKey: string = process.env.NEXT_PUBLIC_SUPABASE_KEY || ''
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
+
 const getSupabase = () => {
     const supabase_ = createClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL,
