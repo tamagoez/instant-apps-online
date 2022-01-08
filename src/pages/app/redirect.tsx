cfaa6d92feb204491e1fa6b43f0f117843f94a13
tamagoes <87574268+tamagoez@users.noreply.github.com>
2022-01-08 11:59:28 +0900

    Update redirect.tsx

--- src/pages/app/redirect.tsx
+++ src/pages/app/redirect.tsx
@@ -1,15 +1,24 @@
 // https://zenn.dev/uttk/articles/4649e49f1e6628
 
+import { NextPage } from 'next'
 import Router from 'next/router'
-import { GetServerSideProps } from 'next'
 
-export default const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    redirect: {
-      statusCode: 302, // ステータスコード指定
-      permanent: false, // 永続的なリダイレクトかどうか
-      destination: '/profile', // リダイレクト先
-      // destination: 'https://example.com/' // 別サイトでも指定可能
-    },
+const RedirectPage: NextPage = () => return {}
+
+RedirectPage.getInitialProps = async ({ res }) => {
+
+  // サーバー側でリダイレクト
+  if (typeof window === 'undefined') {
+    res.writeHead(302, { Location: '/profile' })
+    res.end()
+
+    return {} // 空オブジェクトだと警告文が発生するので注意してください！
   }
+
+  // クライアント側でリダイレクト
+  Router.push('/profile')
+
+  return {} // 同上
 }
+
+export default RedirectPage
